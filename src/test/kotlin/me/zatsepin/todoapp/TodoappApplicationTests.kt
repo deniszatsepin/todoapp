dba6ab4aa51dfbf61005153e42a0abee1591843a
Denis Zatsepin <denis@zatsepin.me>
2020-12-08 21:12:54 +0100

    Initial commit

---
+++ src/test/kotlin/me/zatsepin/todoapp/TodoappApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.zatsepin.todoapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
